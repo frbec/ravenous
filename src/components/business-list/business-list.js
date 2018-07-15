54604948bd24d59c8717ae74e99aa67c258b3158
frbec <fredrikbeckius@gmail.com>
2018-07-16 00:08:52 +0200

    run standard --fix

--- src/components/business-list/business-list.js
+++ src/components/business-list/business-list.js
@@ -1,15 +1,15 @@
-import React from 'react';
+import React from 'react'
 import './business-list.css'
 import Business from '../business/business'
 
 class BusinessList extends React.Component {
-  render() {
+  render () {
     return (
-      <div className="BusinessList">
-      {this.props.businesses.map(business => <Business business={business} />)}
-    </div>
-    );
+      <div className='BusinessList'>
+        {this.props.businesses.map(business => <Business business={business} />)}
+      </div>
+    )
   }
 };
 
-export default BusinessList;
+export default BusinessList
