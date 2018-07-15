8a85ca4604a00251bb50c113d570ff4a46b03d5d
frbec <fredrikbeckius@gmail.com>
2018-07-15 23:57:32 +0200

    add search handler

--- src/components/search-bar/search-bar.js
+++ src/components/search-bar/search-bar.js
@@ -9,8 +9,11 @@ class SearchBar extends React.Component {
       location: '',
       sortBy: 'best_match'
     }
+
     this.handleTermChange = this.handleTermChange.bind(this)
     this.handleLocationChange = this.handleLocationChange.bind(this)
+    this.handleSearch = this.handleSearch.bind(this)
+
     this.sortByOptions = {
       'Best match': 'best_match',
       'Highest rated': 'rating',
@@ -34,6 +37,11 @@ class SearchBar extends React.Component {
     this.setState({ location: e.target.value })
   }
 
+  handleSearch (e) {
+    this.props.searchYelp(this.state.term, this.state.location, this.state.sortBy)
+    e.preventDefault()
+  }
+
   renderSortByOptions () {
     return Object.keys(this.sortByOptions).map(sortByOption => {
       let sortByOptionValue = this.sortByOptions[sortByOption]
@@ -54,7 +62,7 @@ class SearchBar extends React.Component {
           <input onChange={this.handleLocationChange} placeholder='Where?' />
         </div>
         <div className='SearchBar-submit'>
-          <a>Let's Go</a>
+          <a onClick={this.handleSearch}>Let's Go</a>
         </div>
       </div>
     )
